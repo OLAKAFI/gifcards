b4e687f0ef486d7b26e3e82b87b15c08e6e8830c
OLAKAFI <olatunbosun.kafi@gmail.com>
2022-04-01 21:47:00 +0100

    deploy my gifcard to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import CardGifts from './Components/CardGifts';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>GIFT CARDS AND THIER EXCHANGEABLE CURRENCIES</h2>
+      <CardGifts/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/CardContent.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Card, CardGroup} from 'react-bootstrap';
+
+function CardContent({title,currency,id}) {
+  return (
+    <>
+      
+        <CardGroup>
+          <Card style={{ width: '20rem', height: '19rem'}} key={id} className='mb-3 border-primary bg-warning pt-2 '>
+              {/* <Card.Img variant="top" src="holder.js/100px180" className='' /> */}
+              <Card.Body >
+                <Card.Title className='my-auto'>{title}</Card.Title>
+                <Card.Text style={{ height: '8em'}} className='mt-3 p-3 bg-light border border-1 border-rounded fs-5'>
+                  {currency}
+                </Card.Text>
+                <Button variant="primary">Click to know more</Button>
+              </Card.Body>
+          </Card>
+
+
+        </CardGroup>
+          
+
+        
+      
+    </>
+  )
+}
+
+export default CardContent
